ce968ed50373bb6b5cef5a287c072320d743761a
Marcos Bustamante Mateo <sevillamarcoss@gmail.com>
2019-11-25 14:23:50 +0100

    First version

---
+++ precache-manifest.46c6b3b543815336dc7acb60b945cada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a98dd69c4fe243054c09f0c06fb96d",
+    "url": "/React-Typescript-Starter-Kit-Demo/index.html"
+  },
+  {
+    "revision": "d91ca2750c8b2e45856e",
+    "url": "/React-Typescript-Starter-Kit-Demo/static/css/2.ea35eab1.chunk.css"
+  },
+  {
+    "revision": "c1b365caa82b93850937",
+    "url": "/React-Typescript-Starter-Kit-Demo/static/css/main.4e5ea6b5.chunk.css"
+  },
+  {
+    "revision": "d91ca2750c8b2e45856e",
+    "url": "/React-Typescript-Starter-Kit-Demo/static/js/2.713f97f5.chunk.js"
+  },
+  {
+    "revision": "c1b365caa82b93850937",
+    "url": "/React-Typescript-Starter-Kit-Demo/static/js/main.5c0e42be.chunk.js"
+  },
+  {
+    "revision": "d646e87ce9777fc20fde",
+    "url": "/React-Typescript-Starter-Kit-Demo/static/js/runtime-main.dc6d5291.js"
+  }
+]);
